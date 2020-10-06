87bf023500e7045c7fac2e63a4307565a253a36d
Zachary-H <1016368357@qq.com>
2020-10-06 14:27:26 +0800

    2020.10.06

---
+++ 剑指 Offer 10- II. 青蛙跳台阶问题.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numWays(int n) {
+        int mod = 1000000007;
+        if(n <= 1) return 1;
+
+        int dp[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i < n+1; i++)
+        {
+            dp[i] = (dp[i-1] + dp[i-2]) % mod;
+        }
+
+        return dp[n];
+    }
+};
