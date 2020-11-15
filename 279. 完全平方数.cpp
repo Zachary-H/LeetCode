69fea355e2221413bee6579f1c7658dd2104a0e5
Zachary-H <1016368357@qq.com>
2020-11-15 23:41:20 +0800

    2020.11.15

---
+++ 279. 完全平方数.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> res(n+1, INT_MAX);
+        res[0] = 0;
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; i-j*j >= 0; j++)
+            {
+                res[i] = min(res[i], res[i-j*j]+1);
+            }
+        }
+
+        return res[n];
+    }
+};
