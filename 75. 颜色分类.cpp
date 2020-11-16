5b52f8b5a09b7bde93b522d787b67aede87a0104
Zachary-H <1016368357@qq.com>
2020-11-16 21:25:48 +0800

    2020.11.16

---
+++ 75. 颜色分类.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int p0 = 0;
+        int p2 = n - 1;
+
+        for(int i = 0; i <= p2; i++)
+        {
+            while(i <= p2 && nums[i] == 2)
+            {
+                swap(nums[i], nums[p2]);
+                p2--;
+            }
+
+            if(nums[i] == 0)
+            {
+                swap(nums[i], nums[p0]);
+                p0++;
+            }
+        }
+    }
+};
