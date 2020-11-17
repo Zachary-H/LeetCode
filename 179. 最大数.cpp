2e4d5cff430e9660d8df659b74756cb3d0e13a10
Zachary-H <1016368357@qq.com>
2020-11-17 17:53:09 +0800

    2020.11.17

---
+++ 179. 最大数.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static bool cmp(int x, int y)
+    {
+        string sx = to_string(x);
+        string sy = to_string(y);
+
+        string A = sx + sy;
+        string B = sy + sx;
+
+        return A > B;
+    }
+
+    string largestNumber(vector<int>& nums) 
+    {
+        if(nums.size() == 0) return "";
+        if(nums.size() == 1) return to_string(nums[0]);
+
+        string res;
+        sort(nums.begin(), nums.end(), cmp);
+        for(int i : nums)
+        {
+            res += to_string(i);
+        }
+
+        if(res[0] == '0') return "0";
+        return res;
+    }
+};
