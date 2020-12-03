599c90f7d65642d98e3620033d5c4604b607db97
Zachary-H <1016368357@qq.com>
2020-12-03 10:27:31 +0800

    2020.12.03

---
+++ 1005. K 次取反后最大化的数组和.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool cmp(int a, int b)
+    {
+        return abs(a) > abs(b);
+    }
+
+    int largestSumAfterKNegations(vector<int>& A, int K) 
+    {
+        sort(A.begin(), A.end(), cmp);
+        for(int i = 0; i < A.size(); i++)
+        {
+            if(A[i] < 0 && K > 0)
+            {
+                A[i] *= -1;
+                K--;
+            }
+        }
+
+        while(K--) A[A.size() - 1] *= -1;
+
+        int res = 0;
+        for(int i : A) res += i;
+
+        return res;
+    }
+};
