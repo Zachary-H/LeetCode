c93e7a8ab0156b4e436a205cfeb685e150982c71
Zachary-H <1016368357@qq.com>
2021-03-13 11:12:04 +0800

    2021.03.13

---
+++ 114. 二叉树展开为链表.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) 
+    {
+        TreeNode* cur = root;
+        while(cur != nullptr)
+        {
+            if(cur->left != nullptr)
+            {
+                auto next = cur->left;
+                auto pre = next;
+                while(pre->right != nullptr)
+                {
+                    pre = pre->right;
+                }
+                pre->right = cur->right;
+                cur->left = nullptr;
+                cur->right = next;
+            }
+            cur = cur->right;
+        }
+    }
+};
